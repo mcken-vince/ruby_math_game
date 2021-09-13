dca5c5dda6aa50541932f89a7cf80ad78d2026a7
Vincent McKen <mcken.vince@gmail.com>
2021-09-13 15:59:27 +0000

    Initial commit, planning file

---
+++ planning.rb
@@ -0,0 +1,13 @@
+# class Player
+# Variables: lives
+# Methods: ask_question, opponent_correct, opponent_incorrect, lose_life
+# --- Initialize--- @lives = 3
+
+# class Game
+# Variables: players, whose_turn
+# Methods: next_round, declare_winner
+
+# class Question
+# Variables: min, max
+# Method: generate_number, generate_question
+
