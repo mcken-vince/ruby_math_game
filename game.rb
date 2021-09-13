ae0fa1d3717ec2cacd7830f1703f677f578f59de
Vincent McKen <mcken.vince@gmail.com>
2021-09-13 16:11:04 +0000

    Create main file, and files for each class

---
+++ game.rb
@@ -0,0 +1,15 @@
+class Game
+  def initialize(p1, p2)
+    @p1 = p1
+    @p2 = p2
+  end
+
+  def new_game
+    self.next_round
+  end
+
+  def next_round
+    puts "----- NEW TURN -----"
+    
+  end
+end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require './game'
+
+g = Game.new('Player 1', 'Player 2')
+g.new_game
